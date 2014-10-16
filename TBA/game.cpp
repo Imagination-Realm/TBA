158cd61558ce8928ea273ed4e90b721848599d15
borguss2014 <kittelle92@gmail.com>
2014-10-16 19:58:45 +0300

    Commands minor additions

--- TBA/game.cpp
+++ TBA/game.cpp
@@ -12,9 +12,14 @@
 #include <string.h>
 #include <cstdio>
 #include <sstream>
+
+
 std::vector<std::string> Game::levelNames;
 std::string Game::current_level;
 bool Game::gameRunning;
+
+Level *introductionLevel = new Level("introductionLevel");
+Level *levelOne = new Level("levelOne");
 
 void addCommands();
 
@@ -24,7 +29,7 @@ Game::Game(std::string gameTitle, std::string build, std::string release, std::s
 	//Version
 	Input input;
 	input.CenterText("===================================");
-	input.CenterText("====================" + build + " - " + release + " || " + stage + "=====================");
+	input.CenterText("====================" + build + " || " + release + " || " + stage + "====================");
 	input.CenterText("===================================");
 	std::cout << std::endl;
 
@@ -46,8 +51,8 @@ void Game::run()
 	addCommands(); // Adding game commands to the commands list to be displayed by 'help'
 
 	Input input;
-	Level *introductionLevel = new Level("introductionLevel");
-	Level *levelOne = new Level("levelOne");
+	
+	
 
 
 	introductionLevel->setTitle("Introduction");
@@ -55,10 +60,12 @@ void Game::run()
 	introductionLevel->showLevel();
 	current_level = introductionLevel->getLevelName();
 
-	//input.speechMode("Hi there !! What would you like to do next ?? ");
-
 	levelOne->setTitle("Level One");
 	levelOne->setDescription(" Testing level one description");
+
+	//input.speechMode("Hi there !! What would you like to do next ?? ");
+
+	
 
 	while (gameRunning){ //While the game is running , keep receiving user input
 		input.insertCommand();
--- TBA/gameCommands.cpp
+++ TBA/gameCommands.cpp
@@ -1,8 +1,10 @@
 #include "game.h"
 #include <iostream>
 #include <vector>
+#include "level.h"
 
 std::vector<std::string> commands_list;
+extern Level *levelOne;
 
 //Sets gameRunning to false and interrupts the while loop, thus exiting the program
 void exit(){
@@ -11,19 +13,32 @@ void exit(){
 
 //Command 'go'
 void go(std::vector<std::string> words){
-	if (words.size() > 1 && words[1] == "town"){  //If vector 'words' is not empty and its second element is 'town' , then execute command  
-		std::cout << "WORKS" << std::endl;
+	
+	if (words.size() > 1 && words[1] == "levelone"){  //If vector 'words' is not empty and its second element is 'town' , then execute command  
+		levelOne->showLevel();
+		Game::current_level = levelOne->getLevelName();
 	}
 	else { std::cout << "ERROR ! Missing or incorrect argument" << std::endl; }
 }
 
 //Prints a list of available commands
-void help(){
+void help(std::vector<std::string> words){
 
-	for (unsigned int i = 0; i < commands_list.size(); i++){	//Parses the commands_list vector and prints available commands
-		std::cout << commands_list[i] << " ";
+	if (words.size() == 1){
+		std::cout << "Available commands: ";
+		for (unsigned int i = 0; i < commands_list.size(); i++){	//Parses the commands_list vector and prints available commands
+			std::cout << commands_list[i] << " ";
+		}
+		std::cout << std::endl;
+		std::cout << "Check a command's description using: help \"command\"" << std::endl;
 	}
-	std::cout << std::endl;
+	else if (words.size() > 1 && words[1] == "exit"){
+		std::cout << "'exit' quits the game" << std::endl;
+	}
+	else { 
+		std::cout << "Command not found . Write 'help' for a list of available commands" << std::endl;
+	}
+	
 }
 
 
@@ -37,7 +52,7 @@ void check_command_validity(std::vector<std::string> words){
 		go(words);
 	}
 	else if (words[0] == "help"){
-		help();
+		help(words);
 	}
 	else if (words[0] != "") { std::cout << "Error ! Command invalid . Please type 'help' to check for available commands " << std::endl; }
 
@@ -52,7 +67,7 @@ void addCommand(std::string command){
 void addCommands(){
 	addCommand("exit");
 	addCommand("go");
-	addCommand("help");
+	
 }
 
 
